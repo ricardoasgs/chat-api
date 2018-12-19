bba504f52779381e1d085f8ee5828e6dcb4a6ecd
Ricardo Garcia <ricardoaugustogarcia@hotmail.com>
2018-12-19 17:46:05 -0200

    Ajustes socket

--- src/modules/socketEvents.js
+++ src/modules/socketEvents.js
@@ -3,26 +3,31 @@ const chatController = require("../app/controllers/chatControllerSocket");
 module.exports = function(io) {
   // Set socket.io listeners.
   io.on("connection", socket => {
+    console.log("Conectado!!!");
     socket.join("general");
 
     socket.on("getChats", async userId => {
       const chats = await chatController.getChats(userId);
 
-      for (let chat of chats) {
-      }
+      // for (let chat of chats) {
+      //   socket.join(chat._id, function() {
+      //     io.to(chat._id).emit("newMessage", "Connectado ao socket");
+      //   });
+      // }
+      // console.log(socket.rooms);
       socket.emit("chats", chats);
     });
 
     socket.on("joinChats", chat => {
       //console.log(chat);
       socket.join(chat._id, function() {
-        console.log(socket.rooms);
-        io.to(chat._id).emit("newMessage", "3");
+        io.to(chat._id).emit("newMessage", "Connectado ao socket");
       });
+      console.log(socket.rooms);
     });
 
     socket.on("newMessage", async (author, user, messages) => {
-      let rooms = Object.keys(socket.rooms);
+      //let rooms = Object.keys(socket.rooms);
       //console.log(rooms);
       const message = await chatController.sendMessage(author, user, messages);
       console.log(message);
