d208e40ef42c641e02fd231da0a37df0f61e001e
Ricardo Garcia <ricardoaugustogarcia@hotmail.com>
2019-01-11 12:45:56 -0200

    script start

--- src/config/express.js
+++ src/config/express.js
@@ -5,13 +5,15 @@ const cors = require("cors");
 const socketEvents = require("../modules/socketEvents");
 const app = express();
 
-app.use(express.static(__dirname + "/"));
+app.use(express.static(__dirname + "./public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(helmet());
 app.use(cors());
 
-server = app.listen(3003 || process.env.PORT);
+var port = process.env.PORT || 3000;
+
+server = app.listen(port);
 
 const io = require("socket.io").listen(server);
 
